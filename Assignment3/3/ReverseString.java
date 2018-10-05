60a1cae13e11342d285684e9b65bbd16a6eb7be2
Guangyuan Zhao <43185431+GuangyuanZhao1@users.noreply.github.com>
2018-10-05 16:16:52 -0700

    Add files via upload

---
+++ Assignment3/3/ReverseString.java
@@ -0,0 +1,21 @@
+
+public class ReverseString {
+	public String reverse(String s) {
+		s = s.toLowerCase();
+		String[] temp = s.split("[\\s]+");
+		int i;
+		String n = new String();
+		for (i = temp.length - 1; i >= 0; i--) {
+			n = n + temp[i] + " ";
+		}
+		return n;
+	}
+	
+	
+	public static void main(String[] args) {
+		ReverseString rs = new ReverseString();
+		System.out.println(rs.reverse("  The sky is       blue           "));
+		
+	}
+
+}
